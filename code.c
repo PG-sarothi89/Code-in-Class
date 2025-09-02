2bc858c6e7d9f9f766af9e840bc84bde544c3171
PsGhosh <pgsarothi2005@gmail.com>
2025-09-02 09:08:36 +0600

    Refactor code by commenting out unused sections and updating logical operation

--- code.c
+++ code.c
@@ -1,16 +1,29 @@
 
 // Class Date : 02/09/2005
 
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     int a, b, c;
+//     a = 10;
+//     b = 15;
+
+//     printf("Result of a>b is %d\n", a > b);
+//     return 0;
+// }
 
 #include <stdio.h>
-
 int main()
 {
 
-    int a, b, c;
+    int a, b, c, result;
     a = 10;
     b = 15;
+    c = 20;
+    result = (a > b) || (c > a);
+    printf("Result of logical OR is %d\n", result);
 
-    printf("Result of a>b is %d\n", a > b);
     return 0;
 }
