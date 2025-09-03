efbe87316df8237a9a16ca74e91f93a6bf413a29
PsGhosh <pgsarothi2005@gmail.com>
2025-09-03 22:30:56 +0600

    Refactor decrement_Operator.c to improve variable initialization and output clarity

--- increment/decrement_Operator.c
+++ increment/decrement_Operator.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
 
 int main (){
-    int a = 5;
+    int a = 5,b=10;
 
-    printf("Initial value: %d\n", a);
+    printf("Initial value of a: %d\n", a);
+    printf("Initial value of b: %d\n", b);
 
     a++;
-    printf("After increment: %d\n", a);
+    printf("After incrementing a: %d\n", a);
 
     a--;
-    printf("After decrement: %d\n", a);
+    printf("After decrementing a: %d\n", a);
+
+    printf("After incrementing b: %d\n", ++b);
+    printf("After decrementing b: %d\n", --b);
+
+    printf(" the value of a++ + b is: %d\n", a++ + b);
 
     return 0;
 }
