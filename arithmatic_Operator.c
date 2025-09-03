6e683e2c2c36dc7273b565d94a4adbcfe10c26f7
PsGhosh <pgsarothi2005@gmail.com>
2025-09-03 22:25:14 +0600

    Add arithmetic operations implementation in main function

---
+++ arithmatic_Operator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main (){
+
+    int a = 5, b = 10, c;
+
+    c = a + b;
+    printf("Addition: %d\n", c);
+
+    c = a - b;
+    printf("Subtraction: %d\n", c);
+
+    c = a * b;
+    printf("Multiplication: %d\n", c);
+
+    c = b / a;
+    printf("Division: %d\n", c);
+
+    return 0;
+}
