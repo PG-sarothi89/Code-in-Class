a464e7cf4a300ef29a0e9f5eb14fa0ff604c6e1a
PsGhosh <pgsarothi2005@gmail.com>
2025-09-03 22:32:58 +0600

    Add additional arithmetic operations to decrement_Operator.c

--- increment/decrement_Operator.c
+++ increment/decrement_Operator.c
@@ -16,6 +16,9 @@ int main (){
     printf("After decrementing b: %d\n", --b);
 
     printf(" the value of a++ + b is: %d\n", a++ + b);
+    printf(" the value of a + b++ is: %d\n", a + b++);
+    printf(" the value of a + ++b is: %d\n", a + ++b);
+    printf(" the value of ++a + b is: %d\n", ++a + b);
 
     return 0;
 }
